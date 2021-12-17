1a2b8b90d9b548e2e76dd61513a3a69f5e77f3ab
rajucm <raju.mallikarjun.chegaraddi@intel.com>
2021-12-17 10:15:13 +0530

    sensorhal-2.0: adding support to enable/disable sensor_list
    Tracked-On: OAM-100260
    Signed-off-by: rajucm <raju.mallikarjun.chegaraddi@intel.com>
    Signed-off-by: rajani <rajani.ranjan@intel.com>

--- sensors/2.0/iiohal_mediation_v2.0/Sensor.cpp
+++ sensors/2.0/iiohal_mediation_v2.0/Sensor.cpp
@@ -160,7 +160,7 @@ std::vector<Event> Sensor::readEvents() {
         event.u.vec3.y = iioc->devlist[mSensorInfo.sensorHandle].data[1];
         event.u.vec3.z = iioc->devlist[mSensorInfo.sensorHandle].data[2];
     } else {
-        event.u.vec3.x = event.u.vec3.x = event.u.vec3.x = 0;
+        event.u.vec3.x = event.u.vec3.y = event.u.vec3.z = 0;
     }
     event.u.vec3.status = SensorStatus::ACCURACY_HIGH;
     events.push_back(event);
