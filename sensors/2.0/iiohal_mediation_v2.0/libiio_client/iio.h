19426f509364e9784dacc18c0f6da0c79d1ce29e
vilasrk <vilas.r.k@intel.com>
2021-12-30 19:38:48 +0530

    VSOCK Implementation for LibIIO
    libiio: network context support for UNIX domain and virtio sockets
    
    libiio has support for a network context object, which enables connecting
    to an iiod server running on a remote machine via TCP/IP.
    
    For Chrome OS purposes, we want to run iiod over a UNIX socket or a vsock
    to enable either VMs or local processes to connect and manage sensors, but
    not remote devices across the network.
    
    This patch adds support to the server and the client sides of libiio/iiod
    to create UNIX domain and virtio sockets, and establish connections over
    such sockets.
    
    reference:
    https://chromium-review.googlesource.com/c/chromiumos/third_party/libiio/+/1719570
    
    Tracked-On: OAM-100107
    Signed-off-by: vilasrk <vilas.r.k@intel.com>

--- sensors/2.0/iiohal_mediation_v2.0/libiio_client/iio.h
+++ sensors/2.0/iiohal_mediation_v2.0/libiio_client/iio.h
@@ -337,6 +337,17 @@ __api struct iio_context * iio_create_xml_context_mem(
  * @return On failure, NULL is returned and errno is set appropriately */
 __api struct iio_context * iio_create_network_context(const char *host);
 
+/** @brief Create a context from a UNIX socket
+ * @param path Filesystem path to an open UNIX socket
+ * @return On success, a pointer to an iio_context structure
+ * @return On failure, NULL is returned and errno is set appropriately */
+__api struct iio_context * iio_create_unix_context(const char *path);
+
+/** @brief Create a context from a VSOCK
+ * @param port The port number to a listening VSOCK
+ * @return On success, a pointer to an iio_context structure
+ * @return On failure, NULL is returned and errno is set appropriately */
+__api struct iio_context * iio_create_vm_context(const char *port);
 
 /** @brief Create a context from a URI description
  * @param uri A URI describing the context location
