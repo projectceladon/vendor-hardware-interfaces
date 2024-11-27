c3752287016c888fc3699c18bcc42aea7b7c356f
Manvi Bajaj <manvi.bajaj@intel.com>
2024-11-27 12:32:50 +0000

    Upgrade thermal HAL to comply with A15 requirement
    Removed support for android.hardware.thermal AIDL IThermal (@1)
    as it is no longer compatible with the requirements for A15.
    
    Upgraded to android.hardware.thermal AIDL IThermal (@2) to ensure
    compliance with A15 specifications
    
    Tracked-On: OAM-128083
    Signed-off-by: Manvi Bajaj <manvi.bajaj@intel.com>

--- thermal/aidl/default/Thermal.h
+++ thermal/aidl/default/Thermal.h
@@ -66,12 +66,21 @@ class Thermal : public BnThermal {
     ndk::ScopedAStatus unregisterThermalChangedCallback(
             const std::shared_ptr<IThermalChangedCallback>& in_callback) override;
 
+    ndk::ScopedAStatus registerCoolingDeviceChangedCallbackWithType(
+            const std::shared_ptr<ICoolingDeviceChangedCallback>& in_callback,
+            CoolingType in_type) override;
+
+    ndk::ScopedAStatus unregisterCoolingDeviceChangedCallback(
+            const std::shared_ptr<ICoolingDeviceChangedCallback>& in_callback) override;
   private:
     std::mutex thermal_callback_mutex_;
     std::vector<std::shared_ptr<IThermalChangedCallback>> thermal_callbacks_;
     std::thread mCheckThread;
     int mNumCpu;
     std::vector<CallbackSetting> callbacks_;
+    std::mutex cdev_callback_mutex_;
+	std::vector<std::shared_ptr<ICoolingDeviceChangedCallback>> cdev_callbacks_;
+
 };
 
 }  // namespace example
